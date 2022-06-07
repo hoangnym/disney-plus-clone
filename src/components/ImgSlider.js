5419aa4833ea6c61eedf4548fe97543b804b96bf
hoangnym <hoangnym94@gmail.com>
2022-06-07 18:23:37 +0200

    add slider

---
+++ src/components/ImgSlider.js
@@ -0,0 +1,33 @@
+import React from "react";
+import styled from "styled-components";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+
+function ImgSlider() {
+  let settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+  };
+
+  return (
+    <Carousel {...settings}>
+      <Wrap>
+        <img src={process.env.PUBLIC_URL + "/images/slider-badging.jpg"} />
+      </Wrap>
+      <Wrap>
+        <img src={process.env.PUBLIC_URL + "/images/slider-badging.jpg"} />
+      </Wrap>
+    </Carousel>
+  );
+}
+
+export default ImgSlider;
+
+const Carousel = styled(Slider)``;
+
+const Wrap = styled.div``;
