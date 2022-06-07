5fef78f88290432dfd84f7723e8f3531fa5cf7bc
hoangnym <hoangnym94@gmail.com>
2022-06-07 07:59:08 +0200

    add icons to navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,12 +2,67 @@ import React from "react";
 import styled from "styled-components";
 
 function Header() {
-  return <Container>Header</Container>;
+  return (
+    <Nav>
+      <Logo src={process.env.PUBLIC_URL + "/images/logo.svg"} />
+      <NavMenu>
+        <a>
+          <img src={process.env.PUBLIC_URL + "/images/home-icon.svg"} />
+          <span>HOME</span>
+        </a>
+        <a>
+          <img src={process.env.PUBLIC_URL + "/images/search-icon.svg"} />
+          <span>SEARCH</span>
+        </a>
+        <a>
+          <img src={process.env.PUBLIC_URL + "/images/watchlist-icon.svg"} />
+          <span>WATCHLIST</span>
+        </a>
+        <a>
+          <img src={process.env.PUBLIC_URL + "/images/original-icon.svg"} />
+          <span>ORIGINALS</span>
+        </a>
+        <a>
+          <img src={process.env.PUBLIC_URL + "/images/movie-icon.svg"} />
+          <span>MOVIES</span>
+        </a>
+        <a>
+          <img src={process.env.PUBLIC_URL + "/images/series-icon.svg"} />
+          <span>SERIES</span>
+        </a>
+      </NavMenu>
+    </Nav>
+  );
 }
 
 export default Header;
 
-const Container = styled.div`
+const Nav = styled.nav`
   height: 70px;
   background: #090b13;
+  display: flex;
+  align-items: center;
+  padding: 0 36px;
+`;
+
+const Logo = styled.img`
+  width: 80px;
+`;
+
+const NavMenu = styled.div`
+  display: flex;
+
+  a {
+    display: flex;
+    align-items: center;
+    padding: 0 12px;
+    img {
+      height: 20px;
+    }
+  }
+
+  span {
+    font-size: 13px;
+    letter-spacing: 1.42px;
+  }
 `;
