32e6217497bb829fbf598e6c39e088e7e15c6b1c
hoangnym <hoangnym94@gmail.com>
2022-06-08 07:54:50 +0200

    add detail page

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,7 +1,40 @@
 import React from "react";
+import styled from "styled-components";
 
 function Detail() {
-  return <div>Detail</div>;
+  return (
+    <Container>
+      <Background>
+        <img src={process.env.PUBLIC_URL + "/images/bao.jpg"} />
+      </Background>
+      <ImageTitle>
+        <img src={process.env.PUBLIC_URL + "/images/bao.jpg"} />
+      </ImageTitle>
+    </Container>
+  );
 }
 
 export default Detail;
+
+const Container = styled.div`
+  min-height: calc(100vh - 70px);
+  padding: 0 calc(3.5vw + 5px);
+  position: relative;
+`;
+
+const Background = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  z-index: -1;
+  opacity: 0.75;
+  img {
+    height: 100%;
+    width: 100%;
+    object-fit: cover;
+  }
+`;
+
+const ImageTitle = styled.div``;
